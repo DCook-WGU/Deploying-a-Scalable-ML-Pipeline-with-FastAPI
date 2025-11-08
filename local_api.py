35caaa3c131a3605532a102c153d9e9c2dd8bb93
Caprikey <dcoo230@wgu.edu>
2025-11-08 18:10:09 +0000

    local_api.py updates

--- local_api.py
+++ local_api.py
@@ -2,37 +2,94 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
-
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+import argparse
 
 
+def parse_args():
 
-data = {
-    "age": 37,
-    "workclass": "Private",
-    "fnlgt": 178356,
-    "education": "HS-grad",
-    "education-num": 10,
-    "marital-status": "Married-civ-spouse",
-    "occupation": "Prof-specialty",
-    "relationship": "Husband",
-    "race": "White",
-    "sex": "Male",
-    "capital-gain": 0,
-    "capital-loss": 0,
-    "hours-per-week": 40,
-    "native-country": "United-States",
-}
+    parser = argparse.ArgumentParser(
+        description="Train a model using the provided YAML configuration file."
+    )
 
-# TODO: send a POST using the data above
-r = None # Your code here
+    # Configs
+    parser.add_argument(
+        "-m", 
+        "--model",
+        required=False,
+        help="Selected Model",
+    )
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+    return parser.parse_args()
+
+
+def main():
+
+    # Parse Arguements
+    args = parse_args()
+
+    # Load Config via arguments
+    if args.model and args.model.strip():
+        model_name = args.model
+        print(f"Model Found: {model_name}")
+    else:
+        model_name = "random_forest"
+        print(f"No model specified, using default - Random Forest Classifier")
+
+    base_url = "http://127.0.0.1:8000"
+
+    # TODO: send a GET using the URL http://127.0.0.1:8000
+    #r = None # Your code here
+    r = requests.get(base_url)
+
+    # TODO: print the status code
+    # print()
+    print(f"Status Code: {r.status_code}")
+
+    # TODO: print the welcome message
+    # print()
+
+    if r.status_code == 200:
+        data = r.json()
+        print(f"Result: {data.get('message')}")
+    else:
+        print("Get Request failed.")
+
+
+    data = {
+        "age": 37,
+        "workclass": "Private",
+        "fnlgt": 178356,
+        "education": "HS-grad",
+        "education-num": 10,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Prof-specialty",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States",
+    }
+
+    # TODO: send a POST using the data above
+    #r = None # Your code here
+    r = requests.post(f"{base_url}/data/?model={model_name}", json=data)
+
+
+    # TODO: print the status code
+    # print()
+
+    print(f"Status Code: {r.status_code}")
+
+    # TODO: print the result
+    # print()
+
+    if r.status_code == 200:
+        result = r.json()
+        print(f"Result: {result.get('result')}")
+    else:
+        print("Post Request failed.")
+
+if __name__ == "__main__":
+    main()
